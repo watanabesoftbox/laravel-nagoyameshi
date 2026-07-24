2ec7a20c1f1fe44ab4861ce9761cad8aa4d7657b
watanabesoftbox <watasoftdev@gmail.com>
2026-07-25 08:53:41 +0900

    会員側の会員管理機能

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 Route::group(['middleware' => 'guest:admin'], function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
+
+    Route::group(['middleware' => ['auth', 'verified']], function () {
+        Route::resource('user', UserController::class)->only(['index', 'edit', 'update']);
+    });
 });
 
 require __DIR__.'/auth.php';
