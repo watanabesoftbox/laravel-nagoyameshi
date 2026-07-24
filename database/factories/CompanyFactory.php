089d100b8cc28b21b79dafd38d7c5b0a6c1f2ee5
watanabesoftbox <watasoftdev@gmail.com>
2026-07-24 13:14:34 +0900

    管理者側の基本情報設定機能

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Company>
+ */
+class CompanyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => 'テスト',
+            'postal_code' => '0000000',
+            'address' => 'テスト',
+            'representative' => 'テスト',
+            'establishment_date' => 'テスト',
+            'capital' => 'テスト',
+            'business' => 'テスト',
+            'number_of_employees' => 'テスト',
+        ];
+    }
+}
