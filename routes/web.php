f83e85d8bb9e09fe0860104d86c4c7dc35cfe0d5
watanabesoftbox <watasoftdev@gmail.com>
2026-07-25 13:34:20 +0900

    会員側の基本情報表示機能

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,16 @@ use App\Http\Controllers\SubscriptionController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\TermController;
 
 Route::group(['middleware' => 'guest:admin'], function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
 
     Route::resource('restaurants', RestaurantController::class)->only(['index', 'show']);
+
+    Route::get('company', [CompanyController::class, 'index'])->name('company.index');
+    Route::get('terms', [TermController::class, 'index'])->name('terms.index');
 
     Route::group(['middleware' => ['auth', 'verified']], function () {
         Route::resource('user', UserController::class)->only(['index', 'edit', 'update']);
