d68ec2ba089c8fc639d972121873c7044f0928f5
watanabesoftbox <watasoftdev@gmail.com>
2026-07-25 13:46:48 +0900

    管理者側のトップページ

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Restaurant;
 use App\Models\Category;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use App\Models\Restaurant;
+use App\Models\Reservation;
 
 class HomeController extends Controller
 {
@@ -13,7 +16,13 @@ class HomeController extends Controller
         $highly_rated_restaurants = Restaurant::take(6)->get();
         $categories = Category::all();
         $new_restaurants = Restaurant::orderBy('created_at', 'desc')->take(6)->get();
+        $total_users = User::count();
+        $total_premium_users = DB::table('subscriptions')->where('stripe_status', 'active')->count();
+        $total_free_users = $total_users - $total_premium_users;
+        $total_restaurants = Restaurant::count();
+        $total_reservations = Reservation::count();
+        $sales_for_this_month = 300 * $total_premium_users;
 
-        return view('admin.home');
+        return view('admin.home', compact('total_users', 'total_premium_users', 'total_free_users', 'total_restaurants', 'total_reservations', 'sales_for_this_month'));
     }
 }
